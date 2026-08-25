25ac44bd0c3fc65c4a1968ed86df30cea25591b0
felipeAGonzalez <gfelipearmando@gmail.com>
2026-08-25 11:12:03 -0600

    Se agregan adecuaciones para un ambiente productivo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use App\Observers\DialysisPrescriptionObserver;
 use App\Observers\DialysisMonitoringObserver;
 use App\Models\DialysisPrescription;
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Paginator::useBootstrap();
         DialysisPrescription::observe(new DialysisPrescriptionObserver());
         DialysisMonitoring::observe(new DialysisMonitoringObserver());
